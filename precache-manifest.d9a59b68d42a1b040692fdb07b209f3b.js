1d32564026c72eb07947fabcee1fc1632252f7a8
nikos val <nikoss123@windowslive.com>
2020-04-03 19:19:30 +0300

    Updates

---
+++ precache-manifest.d9a59b68d42a1b040692fdb07b209f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c778e25748af2e04c970cfa45aaadb2a",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "46e1ea70ac22e2793d50",
+    "url": "/simple-react-app/static/css/main.6cdaabe9.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/simple-react-app/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-app/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e1ea70ac22e2793d50",
+    "url": "/simple-react-app/static/js/main.32cfec06.chunk.js"
+  },
+  {
+    "revision": "39c33a510b9ed0ee167f",
+    "url": "/simple-react-app/static/js/runtime-main.c32b4fc0.js"
+  }
+]);
